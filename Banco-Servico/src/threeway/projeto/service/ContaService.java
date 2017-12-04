6dc4df0de63b3f8b70c841123a79d6b1d6780187
Erivan <erivan.nas@gmail.com>
2017-12-04 10:40:30 -0200

    mais coisas

--- Banco-Servico/src/threeway/projeto/service/ContaService.java
+++ Banco-Servico/src/threeway/projeto/service/ContaService.java
@@ -17,7 +17,17 @@ public class ContaService {
 	// Debita o valor informado na conta de origem
 	// e adiciona o valor informado na conta de destino
 	public void transferir(Conta contaSaque, double valor, Conta contaDestino ) {
-		this.sacar(contaSaque, valor);
-		this.depositar(contaDestino, valor);
+		this.transferir(contaSaque, valor, contaDestino, 0.0);
+	}
+	
+	public void transferir(Conta contaSaque, double valor, Conta contaDestino, double limite ) {
+		
+		if(contaSaque.getSaldo() + limite < valor ) {
+			System.out.println("Saldo insuficiente!");
+		}
+		else {
+			this.sacar(contaSaque, valor);
+			this.depositar(contaDestino, valor);
+		}	
 	}
 }
